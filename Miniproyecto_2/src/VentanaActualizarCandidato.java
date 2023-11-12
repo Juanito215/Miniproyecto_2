70258e40826943aa43252d6a45184b79c08d14e9
Juan David Cuellar <117455791+Juanito215@users.noreply.github.com>
2023-11-12 14:51:09 -0500

    Implementación visual
     la ventana ya se muestra visualmente para introducir su cedula y buscarla (falta implementar la lógica para que funcione, pero igual ya se puede trabajar sobre ella)

--- Miniproyecto_2/src/VentanaActualizarCandidato.java
+++ Miniproyecto_2/src/VentanaActualizarCandidato.java
@@ -1,5 +1,37 @@
+import java.util.ArrayList;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class VentanaActualizarCandidato extends JFrame {
-    
+    Container contenedor;
+    FlowLayout layout;
+    JLabel etiquetaActualizar;
+    JTextField candidatoActualizar;
+    JButton buscar, actualizar;
+
+    ArrayList<Candidato> listaCandidato;
+    public VentanaActualizarCandidato(ArrayList<Candidato> listaCandidato){
+        this.listaCandidato = listaCandidato;
+        setTitle("Actualizar Candidato");
+        setSize(300, 400);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        contenedor = getContentPane();
+        layout = new FlowLayout();
+        contenedor.setLayout(layout);
+
+        etiquetaActualizar = new JLabel("Introduce la cedula del candidato a actualizar");
+        candidatoActualizar = new JTextField(15);
+        contenedor.add(etiquetaActualizar);
+        contenedor.add(candidatoActualizar);
+
+        buscar = new JButton("Buscar");
+        contenedor.add(buscar);
+
+        
+    }    
 }
